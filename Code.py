63b5c92825656774bfc770c141e57a6813aebe3e
s111060004 <s111060004@mail.nchu.edu.tw>
2024-12-25 15:24:14 +0800

    Added conf. matrix and models to be used

--- Code.py
+++ Code.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import nltk
 from keras.preprocessing.text import Tokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
@@ -8,10 +7,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix
 
-data = pd.read_csv('./Spam_SMS.csv')
 
-data.drop_duplicates(inplace=True)
+data = pd.read_csv('Spam_SMS.csv')
+
+#data.drop_duplicates(inplace=True)
 data.dropna(inplace=True)
 data.describe()
 
@@ -21,6 +24,8 @@ data['Message'] = data['Message'].str.lower()
 tkn = Tokenizer()
 vec = CountVectorizer()
 
+model_DT = DecisionTreeClassifier()
+model_XGB = XGBClassifier()
 model_MNB = MultinomialNB()
 model_LR = LogisticRegression()
 model_SVC = SVC()
@@ -36,6 +41,24 @@ model_LR.fit(X_train, y_train)
 model_SVC.fit(X_train, y_train)
 model_RF.fit(X_train, y_train)
 
+# Get predictions
+y_pred_MNB = model_MNB.predict(X_test)
+y_pred_LR = model_LR.predict(X_test)
+y_pred_SVC = model_SVC.predict(X_test)
+y_pred_RF = model_RF.predict(X_test)
+
+# Calculate confusion matrices
+cm_MNB = confusion_matrix(y_test, y_pred_MNB)
+cm_LR = confusion_matrix(y_test, y_pred_LR)
+cm_SVC = confusion_matrix(y_test, y_pred_SVC)
+cm_RF = confusion_matrix(y_test, y_pred_RF)
+
+print("--- Confusion Matrices ---")
+print(f"Multinomial Naive Bayes: \n{cm_MNB}")
+print(f"Logistic Regression: \n{cm_LR}")
+print(f"Support Vector Machine Classification: \n{cm_SVC}")
+print(f"Random Forest Classification: \n{cm_RF}")
+
 print(f"Accuracy: \n\
     Multinomial Naive Bayes: {model_MNB.score(X_test, y_test)*100:.2f}%, \n\
     Logistic Regression: {model_LR.score(X_test, y_test)*100:.2f}%, \n\
@@ -48,16 +71,4 @@ while True:
         break
     msg = vec.transform([msg])
     pred = [model_MNB.predict(msg), model_LR.predict(msg), model_SVC.predict(msg), model_RF.predict(msg)]
-    pred_results = ""
-    if pred[0] == 1:
-        pred_results += "NB "
-    if pred[1] == 1:
-        pred_results += "LR "
-    if pred[2] == 1:
-        pred_results += "SVC "
-    if pred[3] == 1:
-        pred_results += "RF"
-    if not pred_results:
-        pred_results = "None"
     print(f"{sum(i[0] for i in pred)*100/len(pred)}% flagged as spam")
-    print(f"Flagged by: {pred_results}")
