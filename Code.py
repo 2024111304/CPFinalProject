5e9bdde998e54aa63e3ae18b943cec0d5aafcda3
harper025 <xiao20021009@smail.nchu.edu.tw>
2025-01-01 23:05:46 +0800

    Add files via upload

--- Code.py
+++ Code.py
@@ -1,3 +1,7 @@
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 import pandas as pd
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
